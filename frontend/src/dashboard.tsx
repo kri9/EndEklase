90ef0b2937f9bb994b139db2df132eea0da64b3b
KillReal2418 <therealapplegame@gmail.com>
2024-10-16 19:48:05 +0300

    added login from frontend add new data for backend DataCreator

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="dashboard-wrapper">
+      <h1>Добро пожаловать на тестовую страницу!</h1>
+      <p>TEST PAGE AFTER SUCCESFULLY LOGIN</p>
+    </div>
+  );
+};
+
+export default Dashboard;
