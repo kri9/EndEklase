9cc81f272f09cbc878041759d4767827ae775e8d
KillReal2418 <therealapplegame@gmail.com>
2024-10-22 10:20:55 +0300

    Create attendances with lesson

--- backend/src/main/java/lv/app/backend/mappers/EntityMapper.java
+++ backend/src/main/java/lv/app/backend/mappers/EntityMapper.java
@@ -15,9 +15,12 @@ public interface EntityMapper {
     @Mapping(target = "groupId", source = "group.id")
     @Mapping(target = "kindergartenId", source = "group.kindergarten.id")
     ChildDTO childToDto(Child child);
+
     @Mapping(target = "groupId", source = "group.id")
     LessonDTO lessonToDto(Lesson lesson);
 
     @Mapping(target = "group.id", source = "groupId")
     @Mapping(target = "attendances", ignore = true)
-    Lesson dtoToLesson(LessonDTO lessonDTO);}
+    Lesson dtoToLesson(LessonDTO lessonDTO);
+
+}
--- backend/src/main/java/lv/app/backend/service/LessonService.java
+++ backend/src/main/java/lv/app/backend/service/LessonService.java
@@ -1,10 +1,15 @@
 package lv.app.backend.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import lv.app.backend.dto.LessonDTO;
 import lv.app.backend.mappers.EntityMapper;
+import lv.app.backend.model.Attendance;
 import lv.app.backend.model.Group;
 import lv.app.backend.model.Lesson;
+import lv.app.backend.model.enums.AttendanceStatus;
+import lv.app.backend.model.repository.AttendanceRepository;
 import lv.app.backend.model.repository.GroupRepository;
 import lv.app.backend.model.repository.LessonRepository;
 import org.springframework.stereotype.Service;
@@ -16,33 +21,34 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class LessonService {
 
-    private final LessonRepository lessonRepository;
-    private final GroupRepository groupRepository;
     private final EntityMapper entityMapper;
+    private final GroupRepository groupRepository;
+    private final LessonRepository lessonRepository;
+    private final AttendanceRepository attendanceRepository;
 
+    @Transactional
     public void saveLesson(LessonDTO lessonDTO) {
         Group group = groupRepository.findById(lessonDTO.getGroupId())
                 .orElseThrow(() -> new IllegalArgumentException("Invalid group ID"));
 
         Lesson lesson = entityMapper.dtoToLesson(lessonDTO);
         lesson.setGroup(group);
-
         lessonRepository.saveAndFlush(lesson);
+        group.getChildren().forEach(c ->
+                attendanceRepository.saveAndFlush(Attendance.builder()
+                        .lesson(lesson)
+                        .child(c)
+                        .status(AttendanceStatus.NOT_ATTENDED)
+                        .build())
+        );
     }
-
 
 
     public List<LessonDTO> getLessonsByGroup(Long groupId) {
         List<Lesson> lessons = lessonRepository.findByGroupId(groupId);
         System.out.println("Fetched Lessons for Group ID " + groupId + ": " + lessons);
         return lessons.stream()
-                .map(lesson -> LessonDTO.builder()
-                        .id(lesson.getId())
-                        .topic(lesson.getTopic())
-                        .date(lesson.getDate())
-                        .notes(lesson.getNotes())
-                        .groupId(lesson.getGroup().getId())
-                        .build())
+                .map(entityMapper::lessonToDto)
                 .collect(Collectors.toList());
     }
 
