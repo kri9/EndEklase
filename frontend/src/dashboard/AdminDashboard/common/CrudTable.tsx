73e0c6b912f5bb35b399d95bdd11f1583b802d14
KillReal2418 <therealapplegame@gmail.com>
2025-12-02 16:01:25 +0200

    fixed problem with table (dark mode)

--- frontend/src/dashboard/AdminDashboard/common/CrudTable.tsx
+++ frontend/src/dashboard/AdminDashboard/common/CrudTable.tsx
@@ -123,8 +123,8 @@ export default function CrudTable<T extends IdSupplier>(
   }
 
   return (
-    <table className="table-bordered mt-5 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <table className="table table-bordered mt-5 w-full text-sm text-left rtl:text-right text-gray-900">
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50">
         <tr>
           {columns.map((c, i) => (
             <th key={i} scope="col" className="px-6 py-3">
